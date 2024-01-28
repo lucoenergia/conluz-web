40d0cae1654220b993f96a6fb923a6579080e0ed
Víctor Cañizares <victorcm@iriusrisk.com>
2024-01-28 20:44:58 +0100

    [CONLUZWEB-4] Moved folders out of /modules

--- src/app/modules/error/page.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-
-// ** Component Import
-import Error401 from "./components/401";
-
-const ErrorPage = () => <Error401 />;
-
-export default ErrorPage;
--- src/app/modules/form-layouts/layout.tsx
+++
@@ -1,6 +0,0 @@
-// ** Layout Import
-import MainLayout from "@/app/shared/layouts/main/MainLayout";
-
-export default function Layout({ children }: { children: React.ReactNode }) {
-  return <MainLayout>{children}</MainLayout>;
-}
