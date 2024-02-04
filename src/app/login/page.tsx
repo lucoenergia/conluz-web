2e8489422524323035afec0215fe9e6fbe2952c2
viktorKhan <victor.canizares.mata@gmail.com>
2024-02-04 19:12:31 +0100

    [CONLUZWEB-3] Removed unused references to social media icons

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -27,10 +27,6 @@ import MuiFormControlLabel, {
 } from "@mui/material/FormControlLabel";
 
 // ** Icons Imports
-import Google from "mdi-material-ui/Google";
-import Github from "mdi-material-ui/Github";
-import Twitter from "mdi-material-ui/Twitter";
-import Facebook from "mdi-material-ui/Facebook";
 import EyeOutline from "mdi-material-ui/EyeOutline";
 import EyeOffOutline from "mdi-material-ui/EyeOffOutline";
 
