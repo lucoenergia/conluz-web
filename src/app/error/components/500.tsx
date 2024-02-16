67383c35a197674a83aba4240f7e376496c24dcb
Adnana Dragut <100771408+ACD03@users.noreply.github.com>
2024-02-16 14:14:34 +0100

    Feature/conluzweb 15 (#23)
    * Create not-found.tsx page
    
    * Add Error404 component to not-found.tsx page
    
    * Updating page 404.tsx
    
    * Styling Unexpected error found page
    
    * Changing 401 error component with 500 error component for testing purposes
    
    * Removing unnecessary image
    
    * Removing changes from another PR
    
    * Changing, title and subtitle
    
    * Adding contant button
    
    * Fixing the incorrect position of the grid closing tag
    
    * Changing the 500 error image

--- src/app/error/components/500.tsx
+++ src/app/error/components/500.tsx
@@ -5,42 +5,12 @@ import Link from "next/link";
 
 // ** MUI Components
 import Button from "@mui/material/Button";
-import { styled } from "@mui/material/styles";
 import Typography from "@mui/material/Typography";
-import Box, { BoxProps } from "@mui/material/Box";
-
-// ** Demo Imports
-import FooterIllustrations from "@/app/shared/components/footer-illustrations/misc/FooterIllustrations";
+import Box from "@mui/material/Box";
+import { Grid } from "@mui/material";
 
 // ** Styled Components
-const BoxWrapper = styled(Box)<BoxProps>(({ theme }) => ({
-  [theme.breakpoints.down("md")]: {
-    width: "90vw",
-  },
-}));
-
-const Img = styled("img")(({ theme }) => ({
-  marginBottom: theme.spacing(10),
-  [theme.breakpoints.down("lg")]: {
-    height: 450,
-    marginTop: theme.spacing(10),
-  },
-  [theme.breakpoints.down("md")]: {
-    height: 400,
-  },
-  [theme.breakpoints.up("lg")]: {
-    marginTop: theme.spacing(13),
-  },
-}));
-
-const TreeIllustration = styled("img")(({ theme }) => ({
-  left: 0,
-  bottom: "5rem",
-  position: "absolute",
-  [theme.breakpoints.down("lg")]: {
-    bottom: 0,
-  },
-}));
+import * as Styled from "./500.styles";
 
 const Error500 = () => {
   return (
@@ -54,28 +24,63 @@ const Error500 = () => {
           textAlign: "center",
         }}
       >
-        <BoxWrapper>
-          <Typography variant="h1">500</Typography>
+        <Styled.BoxWrapper>
           <Typography
-            variant="h5"
-            sx={{ mb: 1, fontSize: "1.5rem !important" }}
+            variant="h1"
+            sx={{
+              mb: 2.5,
+              fontSize: { md: "2rem !important", xs: "1.5rem !important" },
+            }}
           >
-            Internal server error 👨🏻‍💻
+            No se ha podido procesar tu petición
           </Typography>
-          <Typography variant="body2">Oops, something went wrong!</Typography>
-        </BoxWrapper>
-        <Img
+          <Typography variant="body1">
+            Lo sentimos, no hemos podido procesar tu petición correctamente. Por
+            favor, vuelve a intentarlo más tarde. Si el problema persiste,
+            contáctanos.
+          </Typography>
+        </Styled.BoxWrapper>
+        <Styled.Img
           height="487"
           alt="error-illustration"
           src="/images/pages/500.png"
         />
-        <Button href="/" component={Link} variant="contained" sx={{ px: 5.5 }}>
-          Back to Home
-        </Button>
+        <Grid
+          container
+          spacing={{ xs: 2, md: 2 }}
+          columns={{ xs: 2, sm: 8, md: 12 }}
+          justifyContent="center"
+        >
+          <Grid item xs={6} sm={4} md={4}>
+            <Button
+              href="/dashboard"
+              component={Link}
+              variant="contained"
+              sx={{
+                px: 5.5,
+                width: { xs: "60vw", sm: "auto" },
+                fontWeight: "bold",
+              }}
+            >
+              Volver a la página principal
+            </Button>
+          </Grid>
+          <Grid item xs={6} sm={4} md={4}>
+            <Button
+              href="/help"
+              component={Link}
+              variant="contained"
+              sx={{
+                px: 5.5,
+                width: { xs: "60vw", sm: "auto" },
+                fontWeight: "bold",
+              }}
+            >
+              Ir a la página de contacto
+            </Button>
+          </Grid>
+        </Grid>
       </Box>
-      <FooterIllustrations
-        image={<TreeIllustration alt="tree" src="/images/pages/tree-3.png" />}
-      />
     </Box>
   );
 };
