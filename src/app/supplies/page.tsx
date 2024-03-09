2eedc781f9cf0342e279f8cff39e62de8fe383ea
Adnana Dragut <catrinel21@gmail.com>
2024-03-09 16:03:50 +0100

    created page and layout for the supplies list view

---
+++ src/app/supplies/page.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+// ** React Imports
+import * as React from "react";
+
+// ** MUI Imports
+import Grid from "@mui/material/Grid";
+
+// ** Demo Components Imports
+
+const MUITable = () => {
+  return <Grid container spacing={6}></Grid>;
+};
+
+export default MUITable;
