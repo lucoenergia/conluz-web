3eed5bbb23c0d2371595693bc72cc5aac8d1f826
viktorKhan <victor.canizares.mata@gmail.com>
2024-07-03 23:46:21 +0200

    [conluzweb-38] Configured chart component to load data from server through API rest call

--- src/app/charts/components/BasicChart.tsx
+++ src/app/charts/components/BasicChart.tsx
@@ -1,31 +1,84 @@
 'use client';
 
+import { useEffect, useState } from "react";
+
 import dynamic from 'next/dynamic';
+import { ApexOptions } from 'apexcharts';
+
+import apiClient from "../../shared/restApi/apiClient";
 
 // Dynamically import ApexCharts with SSR disabled for Next.js compatibility
 const ApexChart = dynamic(() => import('react-apexcharts'), { ssr: false });
 
-export function BasicChart(){
+// Declare the type for chart options
+type ChartOptions = {
+  chart: {
+    id: string;
+  };
+  xaxis: {
+    type: string;
+  },
+  yaxis: {
+    opposite: boolean;
+  },
+};
 
-    const option = {
-        chart: {
-          id: 'apexchart-example'
-        },
-        xaxis: {
-          categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999]
+// Declare the type for series
+type Series = {
+  name: string;
+  data: number[];
+}[];
+
+export function BasicChart() {
+
+  const [option, setOption] = useState<ApexOptions | null>(null);
+  const [series, setSeries] = useState<[number, number][]>([]);
+
+  useEffect(() => {
+    const fetchPrices = async () => {
+      try {
+        const response = await apiClient.get("/prices", {
+          params: {
+            startDate: "2024-06-15T00:00:00.000+02:00",
+            endDate: "2024-06-15T23:59:00.000+02:00"
+          }
+        });
+
+        if (response.status === 200) {          
+
+          setSeries(
+            response.data.map((item: any) => [new Date(item.hour).getTime(), item.price])
+          );
+
+          const options: ApexOptions = {
+            chart: {
+              id: "chart-prices",
+            },
+            xaxis: {
+              type: 'datetime'
+            },
+            yaxis: {
+              opposite: true
+            },
+          };
+
+          setOption(options);
         }
+      } catch (error) {
+        console.error("Failed to fetch data from API: ", error);
       }
+    };
 
-    const series = [{
-        name: 'series-1',
-        data: [30, 40, 35, 50, 49, 60, 70, 91, 125]
-      }]
+    fetchPrices();
+  }, []);
 
-    return(
-        <>
-            <ApexChart type="line" options={option} series={series} height={200} width={500} />
-        </>
-    )
+  console.log("series: " + series)
+
+  return option && series.length > 0 ? (
+    <ApexChart type="line" options={option} series={[{ data: series }]} height={200} width={500} />
+  ) : (
+    <p>Loading...</p>
+  );
 }
 
 export default BasicChart;
