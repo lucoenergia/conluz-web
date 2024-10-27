478948dbfd6dd6d0fd4c67412394b9fa20bb58bc
viktorKhan <victor.canizares.mata@gmail.com>
2024-10-27 13:34:45 +0100

    [conluzweb-42] Created a panel with a chart line showing energy prices as a first example

---
+++ src/app/dashboard/components/ProductionSummary.tsx
@@ -0,0 +1,152 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+// ** MUI Imports
+import Box from "@mui/material/Box";
+import Card from "@mui/material/Card";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import CardContent from "@mui/material/CardContent";
+import { styled, useTheme } from "@mui/material/styles";
+
+import apiClient from "../../shared/restApi/apiClient";
+
+import dynamic from 'next/dynamic';
+import { ApexOptions } from "apexcharts";
+// Dynamically import ApexCharts with SSR disabled for Next.js compatibility
+const ApexChart = dynamic(() => import('react-apexcharts'), { ssr: false });
+
+const ProductionSummary = () => {
+  // ** Hook
+  // const theme = useTheme();
+
+  const chartOptions: ApexOptions = {
+    chart: {
+      id: "chart-prices",
+      defaultLocale: 'en',
+      dropShadow: {
+        enabled: true,
+        color: '#000',
+        top: 18,
+        left: 7,
+        blur: 10,
+        opacity: 0.2
+      },
+    },
+    plotOptions: {
+      bar: {
+        borderRadius: 3,
+        dataLabels: {
+          position: 'top', // top, center, bottom
+        },
+      }
+    },
+    dataLabels: {
+      enabled: false
+    },
+    grid: {
+      row: {
+        colors: ['#e5e5e5', 'transparent'],
+        opacity: 0.5
+      }, 
+      column: {
+          colors: ['#f8f8f8', 'transparent'],
+      },
+      xaxis: {
+        lines: {
+          show: true
+        }
+      }
+    },
+    colors: ['rgb(238, 177, 17)'],
+    markers: {
+      size: 1
+    },
+    stroke: {
+      curve: 'smooth'
+    },
+    xaxis: {
+      type: 'category',
+      labels: {
+        formatter: function (value) {
+          console.log("vcm: " + value)
+          const date = new Date(value);
+
+          // Format the date and time
+          const dateString = date.toLocaleDateString('es-ES', { day: '2-digit', month: '2-digit' });
+          const timeString = date.toLocaleTimeString('es-ES', { hour: '2-digit', minute: '2-digit', hour12: false });
+
+          return dateString + ' ' + timeString;
+        }
+      },
+      tooltip: {
+        enabled: false
+      },
+    },
+    yaxis: {
+      type: 'category',
+      title: {
+        text: 'Precio'
+      },
+      min: 0,
+      labels: {
+        formatter: function (value) {
+          return value + "€";
+        }
+      },
+    },
+  };
+
+  const [series, setSeries] = useState<[number, number][]>([]);
+
+  useEffect(() => {
+    const fetchPrices = async () => {
+      try {
+        const response = await apiClient.get("/prices", {
+          params: {
+            startDate: "2024-06-15T00:00:00.000+02:00",
+            endDate: "2024-06-15T23:59:00.000+02:00"
+          }
+        });
+
+        if (response.status === 200) {
+
+          setSeries(
+            response.data.map((item: any) => {
+              const price = (item.price < 0) ? 0.0 : parseFloat(parseFloat(item.price)).toFixed(2);
+              return [new Date(item.hour).getTime(), price]
+            })
+          );
+        }
+      } catch (error) {
+        console.error("Failed to fetch prices: ", error);
+      }
+    };
+
+    fetchPrices();
+  }, []);
+
+  return (
+    <Card sx={{ position: "relative" }}>
+      <CardContent>
+        <Typography variant="h6">Producción</Typography>
+        <Typography variant="body2" sx={{ letterSpacing: "0.25px" }}>
+          Energía generada por la comunidad energética.
+        </Typography>
+        <CardContent
+          sx={{ "& .apexcharts-xcrosshairs.apexcharts-active": { opacity: 0 } }}
+        >
+          <ApexChart type="line" options={chartOptions} series={[{ name: 'Producción', data: series }]} height={350} width={700} />
+          <Box sx={{ mb: 7, display: "flex", alignItems: "center" }}>
+          </Box>
+          <Button fullWidth variant="contained">
+            Ver todos los datos de producción
+          </Button>
+        </CardContent>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default ProductionSummary;
