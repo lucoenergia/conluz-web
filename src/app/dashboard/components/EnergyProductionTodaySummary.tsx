33d2e9022a7aa193e00d9475ded2d97f120c3402
Víctor Cañizares Mata <victor.canizares.mata@gmail.com>
2024-10-28 23:30:51 +0100

    [conluzweb-43] Implemented a new component to display production data… (#45)
    * [conluzweb-43] Implemented a new component to display production data for today
    
    * [conluzweb-43] Removed traces with console.log

---
+++ src/app/dashboard/components/EnergyProductionTodaySummary.tsx
@@ -0,0 +1,158 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+// ** MUI Imports
+import Box from "@mui/material/Box";
+import Card from "@mui/material/Card";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import CardContent from "@mui/material/CardContent";
+import Link from "@mui/material/Link";
+
+import apiClient from "../../shared/restApi/apiClient";
+import { getTodayStartOfDay, getTodayEndOfDay, formatDate, getHourFromStringDatetime } from "../../shared/date/dateUtil";
+
+
+import dynamic from 'next/dynamic';
+import { ApexOptions } from "apexcharts";
+import chartI18nLocale from '../../shared/charts/apexcharts/i18n/es.json';
+// Dynamically import ApexCharts with SSR disabled for Next.js compatibility
+const ApexChart = dynamic(() => import('react-apexcharts'), { ssr: false });
+
+const EnergyProductionTodaySummary = () => {
+  // ** Hook
+  // const theme = useTheme();
+
+  const chartOptions: ApexOptions = {
+    chart: {
+      id: "chart-production",
+      locales: [chartI18nLocale],
+      defaultLocale: 'es',
+      dropShadow: {
+        enabled: true,
+        color: '#000',
+        top: 18,
+        left: 7,
+        blur: 10,
+        opacity: 0.2
+      },
+    },
+    plotOptions: {
+      bar: {
+        borderRadius: 3,
+        dataLabels: {
+          position: 'top', // top, center, bottom
+        },
+      }
+    },
+    dataLabels: {
+      enabled: false
+    },
+    grid: {
+      row: {
+        colors: ['#e5e5e5', 'transparent'],
+        opacity: 0.5
+      },
+      column: {
+        colors: ['#f8f8f8', 'transparent'],
+      },
+      xaxis: {
+        lines: {
+          show: true
+        }
+      }
+    },
+    colors: ['rgb(238, 177, 17)'],
+    markers: {
+      size: 1
+    },
+    stroke: {
+      curve: 'smooth'
+    },
+    xaxis: {
+      type: 'category',
+      labels: {
+        formatter: function (value) {
+          return Math.ceil(value) + "h";
+        }
+      },
+      min: 0,
+      max: 23,
+      tickAmount: 6,
+      tooltip: {
+        enabled: false
+      },
+    },
+    yaxis: {
+      type: 'category',
+      title: {
+        text: 'Energía producida en kWh'
+      },
+      min: 0,
+      max: 65,
+      labels: {
+        formatter: function (value) {
+          return value + "kWh";
+        }
+      },
+    },
+  };
+
+  const [series, setSeries] = useState<[number, number][]>([]);
+
+  useEffect(() => {
+    const fetchProduction = async () => {
+      try {
+        const startDate = getTodayStartOfDay();
+        const endDate = getTodayEndOfDay();
+
+        const response = await apiClient.get("/production/hourly", {
+          params: {
+            startDate: startDate, // Example: "2024-06-15T00:00:00.000+02:00"
+            endDate: endDate // Example: "2024-06-15T23:59:00.000+02:00"
+          }
+        });
+
+        if (response.status === 200) {
+
+          setSeries(
+            response.data.map((item: any) => {
+              const power = (item.power < 0) ? 0.0 : parseFloat(parseFloat(item.power)).toFixed(2);
+              const time = getHourFromStringDatetime(item.time)
+              return [time, power]
+            })
+          );
+        }
+      } catch (error) {
+        console.error("Failed to fetch energy production for today: ", error);
+      }
+    };
+
+    fetchProduction();
+  }, []);
+
+  return (
+    <Card sx={{ position: "relative" }}>
+      <CardContent>
+        <Typography variant="h6">Energía producida el {formatDate(new Date(), 'DD/MM/yyyy')}</Typography>
+        <Typography variant="body2" sx={{ letterSpacing: "0.25px" }}>
+          {`Energía producida por la comunidad energética`}
+          .
+        </Typography>
+        <CardContent
+          sx={{ "& .apexcharts-xcrosshairs.apexcharts-active": { opacity: 0 } }}
+        >
+          <ApexChart type="bar" options={chartOptions} series={[{ name: 'Energía en kWh', data: series }]} width={"100%"} height={350} />
+          <Box sx={{ mb: 7, display: "flex", alignItems: "center" }}>
+          </Box>
+          <Button fullWidth variant="contained">
+            Ver producción de energía
+          </Button>
+        </CardContent>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default EnergyProductionTodaySummary;
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -18,22 +18,23 @@ import ApexChartWrapper from "@/app/shared/styles/libs/react-apexcharts/ApexChar
 // ** Demo Components Imports
 import Table from "./components/Table";
 // import Trophy from "./components/Trophy";
-import EnergyPricesSummary from "./components/EnergyPricesSummary";
+import EnergyPricesTodaySummary from "./components/EnergyPricesTodaySummary";
 import TotalEarning from "./components/TotalEarning";
 import StatisticsCard from "./components/StatisticsCard";
 import WeeklyOverview from "./components/WeeklyOverview";
 import DepositWithdraw from "./components/DepositWithdraw";
 import SalesByCountries from "./components/SalesByCountries";
+import EnergyProductionTodaySummary from "./components/EnergyProductionTodaySummary";
 
 const Dashboard = () => {
   return (
     <ApexChartWrapper>
       <Grid container spacing={6}>
         <Grid item xs={12} md={6}>
-          <EnergyPricesSummary />
+          <EnergyPricesTodaySummary />
         </Grid>
         <Grid item xs={12} md={6}>
-          <StatisticsCard />
+          <EnergyProductionTodaySummary />
         </Grid>
         {/* <Grid item xs={12} md={6} lg={4}>
           <WeeklyOverview />
