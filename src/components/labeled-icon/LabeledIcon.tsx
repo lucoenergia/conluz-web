e99c778d8c6692b74cf4e8ce4839924912f0bb6a
éoj <eoj@riseup.net>
2025-07-03 15:23:54 +0200

    [conluzweb-31] Added LabeledIcon component

---
+++ src/components/labeled-icon/LabeledIcon.tsx
@@ -0,0 +1,24 @@
+import type { FC } from "react";
+import type { SvgIconComponent } from '@mui/icons-material';
+
+interface LabeledIconProps {
+  icon: SvgIconComponent,
+  iconPosition?: 'left' | 'right'
+  justify?: 'start' | 'end' | 'between'
+  label: string
+}
+
+const contentJustification = {
+  start: "justify-start",
+  end: "justify-end",
+  between: "justify-between"
+}
+
+export const LabeledIcon: FC<LabeledIconProps> = ({ label, icon: Icon, iconPosition='left', justify='start' }) => {
+  return <div className={`grid grid-flow-col w-full ${contentJustification[justify]} gap-2`}>
+    { iconPosition==='left' && <Icon/> }
+    <span>{ label }</span>
+    { iconPosition==='right' && <Icon/> }
+  </div>
+}
+
