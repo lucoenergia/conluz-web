004e5b75a9200a7130df7dc9803ad5f532f5f8f5
éoj <eoj@riseup.net>
2025-07-03 16:47:28 +0200

    [conluzweb-23] Added ProfileMenu component with tests

---
+++ src/components/header/ProfileMenu.spec.tsx
@@ -0,0 +1,38 @@
+import '@testing-library/jest-dom'
+import { expect, test } from "vitest";
+import { ProfileMenu } from "./ProfileMenu";
+import { screen, render } from "@testing-library/react";
+import { MemoryRouter } from "react-router";
+import userEvent from '@testing-library/user-event'
+
+test("ProfileMenu renders correctly with menu hidden", async () => {
+  render(<MemoryRouter><ProfileMenu username="Luis Mata"/></MemoryRouter>)
+  expect(screen.getByText('Luis Mata')).not.toBeVisible();
+  expect(screen.getByText('Mi perfil')).not.toBeVisible();
+  expect(screen.getByText('Cambiar contraseña')).not.toBeVisible();
+  expect(screen.getByText('¿Necesitas ayuda?')).not.toBeVisible();
+  expect(screen.getByText('Salir')).not.toBeVisible();
+})
+
+test("ProfileMenu opens menu when clicking", async () => {
+  const user = userEvent.setup();
+  render(<MemoryRouter><ProfileMenu username="Luis Mata"/></MemoryRouter>)
+  await user.click(screen.getByRole('button'));
+  expect(screen.getByText('Luis Mata')).toBeVisible();
+  expect(screen.getByText('Mi perfil')).toBeVisible();
+  expect(screen.getByText('Cambiar contraseña')).toBeVisible();
+  expect(screen.getByText('¿Necesitas ayuda?')).toBeVisible();
+  expect(screen.getByText('Salir')).toBeVisible();
+})
+
+test("ProfileMenu closes menu when removing focus", async () => {
+  const user = userEvent.setup();
+  render(<MemoryRouter><ProfileMenu username="Luis Mata"/></MemoryRouter>)
+  await user.click(screen.getByRole('button'));
+  await user.keyboard('{Escape}')
+  expect(screen.getByText('Luis Mata')).not.toBeVisible();
+  expect(screen.getByText('Mi perfil')).not.toBeVisible();
+  expect(screen.getByText('Cambiar contraseña')).not.toBeVisible();
+  expect(screen.getByText('¿Necesitas ayuda?')).not.toBeVisible();
+  expect(screen.getByText('Salir')).not.toBeVisible();
+})
---
+++ src/components/header/ProfileMenu.tsx
@@ -0,0 +1,82 @@
+import { Avatar, Divider, IconButton, Menu, MenuItem } from "@mui/material";
+import { useState, type FC } from "react";
+import { MenuLinkItem } from "../menu/MenuLinkItem";
+import { LabeledIcon } from "../labeled-icon/LabeledIcon";
+import PersonIcon from '@mui/icons-material/Person';
+import KeyIcon from '@mui/icons-material/Key';
+import HelpOutlineIcon from '@mui/icons-material/HelpOutline';
+import LogoutIcon from '@mui/icons-material/Logout';
+
+interface ProfileMenuProps {
+  username: string
+}
+
+export const ProfileMenu: FC<ProfileMenuProps> = ({ username }) => {
+  const [anchorElement, setAnchorElement] = useState<HTMLElement | null>(null);
+
+  const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElement(event.currentTarget);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElement(null);
+  };
+  
+  return <>
+    <IconButton sx={{ p: 0 }} onClick={handleOpenUserMenu}>
+      <Avatar alt="Icono de usuario" />
+    </IconButton>
+    <Menu
+      sx={{ mt: '45px' }}
+      anchorOrigin={{
+        vertical: 'top',
+        horizontal: 'right',
+      }}
+      keepMounted
+      transformOrigin={{
+        vertical: 'top',
+        horizontal: 'right',
+      }}
+      anchorEl={anchorElement}
+      open={Boolean(anchorElement)}
+      onClose={handleCloseUserMenu}
+    >
+      <MenuItem>{ username }</MenuItem>
+      <Divider />
+      <MenuLinkItem to="/profile">
+        <LabeledIcon
+          justify="between"
+          iconPosition="right"
+          icon={PersonIcon}
+          label="Mi perfil"
+        />
+      </MenuLinkItem>
+      <MenuItem>
+        <LabeledIcon
+          justify="between"
+          iconPosition="right"
+          icon={KeyIcon}
+          label="Cambiar contraseña"
+        />
+      </MenuItem>
+      <Divider />
+      <MenuLinkItem to="/contact">
+        <LabeledIcon
+          justify="between"
+          iconPosition="right"
+          icon={HelpOutlineIcon}
+          label="¿Necesitas ayuda?"
+        />
+      </MenuLinkItem>
+      <Divider />
+      <MenuItem>
+        <LabeledIcon
+          justify="between"
+          iconPosition="right"
+          icon={LogoutIcon}
+          label="Salir"
+        />
+      </MenuItem>
+    </Menu>
+  </>
+}
