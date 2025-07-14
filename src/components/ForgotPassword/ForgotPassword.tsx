9815778a663103761887d845c0329e51f7112f33
Natalia M Zaballos <natmarzab@proton.me>
2025-07-14 10:23:22 +0200

    [conluzweb-11] forgotpassword and new password error messages added

--- src/components/ForgotPassword/ForgotPassword.tsx
+++ src/components/ForgotPassword/ForgotPassword.tsx
@@ -1,18 +1,28 @@
 import { Box, Button, FormGroup, TextField, Typography } from "@mui/material"
-import type { FC } from "react"
-
-const handleSubmit = (event: React.FormEvent) => {
-event.preventDefault();
-};
-// const idErrorMessage = 'Por favor, introduce tu DNI/NIF'
+import { useState, type FC } from "react"
 
 export const ForgotPassword: FC = () => {
-return <Box component="form" className="p-7 w-full" onSubmit={handleSubmit}>
+    const [formError, setFormError] = useState<boolean>(false);
+    const invalidIdFormat = 'El DNI/NIF debe tener 7 dígitos y una letra';
+    const idErrorMessage = 'Este DNI no se encuentra en nuestra base de datos. Por favor, introdúcelo de nuevo. Si el problema persiste ponte en contacto con nosotres.';
+
+    const isValidIdFormat = (value: string) => /^\d{7}[a-zA-Z]$/.test(value.trim());
+
+    const handleSubmit = async (data: FormData) => {
+        const id = data.get('id')?.toString().trim();
+        const newError = !id || !isValidIdFormat(id);
+        
+        setFormError(newError);
+        if (newError) return;
+
+    };
+return <Box component="form" className="p-7 w-full" action={handleSubmit}>
         <Typography  gutterBottom className="text-xl">¿No recuerdas tu contraseña?</Typography>
         <Typography  gutterBottom className="text-base">Introduce tu DNI y te enviaremos un correo</Typography>
         <FormGroup className="mt-6">
-            <TextField                // error={emailError}
-                // helperText={idErrorMessage}
+            <TextField                
+                error={formError}
+                helperText={formError? invalidIdFormat : ''}
                 id="id"
                 type="text"
                 name="id"
@@ -21,7 +31,6 @@ return <Box component="form" className="p-7 w-full" onSubmit={handleSubmit}>
                 required
                 fullWidth
                 variant="filled"
-                // color={emailError ? 'error' : 'primary'}
                 ></TextField>
                 <Button type="submit" variant="contained" fullWidth className="mt-4">Enviar</Button>
         </FormGroup>
--- src/components/NewPassword/NewPassword.tsx
+++ src/components/NewPassword/NewPassword.tsx
@@ -1,17 +1,40 @@
 import { Box, Button, FormGroup, TextField, Typography } from "@mui/material"
-import type { FC } from "react"
+import { useState, type FC } from "react"
 
-const handleSubmit = (event: React.FormEvent) => {
-event.preventDefault();
-};
 
 export const NewPassword: FC = () => {
-return <Box component="form" className="p-7 w-full" onSubmit={handleSubmit}>
+    const [formErrors, setFormErrors] = useState<{
+        password1: boolean, 
+        password2: boolean, 
+        differentPasswords: boolean}>({
+            password1: false, 
+            password2: false, 
+            differentPasswords: false});
+
+    
+    const differentPasswordMessage = 'La contraseña no coincide';
+    const passwordReminder = 'Por favor, repite tu contraseña';
+    const newPasswordMessage = 'Introduce tu nueva contraseña';
+
+
+    const handleSubmit = async (data: FormData) => {
+        const password1 = data.get('newPassword')?.toString().trim();
+        const password2 = data.get('repeatNewPassword')?.toString().trim();
+        const newErrors = {
+            password1 : !password1,
+            password2 : !password2,
+            differentPasswords : password2 !== password1 
+        }
+        setFormErrors(newErrors);
+        if (newErrors.password1 || newErrors.password2 || newErrors.differentPasswords ) return;
+
+    };
+return <Box component="form" className="p-7 w-full" action={handleSubmit}>
         <Typography  gutterBottom className="text-base">Introduce tu nueva contraseña</Typography>
         <FormGroup >
             <TextField                
-                // error={emailError}
-                // helperText={emailErrorMessage}
+                error={formErrors.password1}
+                helperText={formErrors.password1 ? newPasswordMessage : ''}
                 id="newPassword"
                 type="password"
                 name="newPassword"
@@ -21,20 +44,20 @@ return <Box component="form" className="p-7 w-full" onSubmit={handleSubmit}>
                 fullWidth
                 variant="filled"
                 // color={emailError ? 'error' : 'primary'}
-                ></TextField>
-                        <TextField                
-                // error={emailError}
-                // helperText={emailErrorMessage}
+                />
+                <TextField                
+                error={formErrors.password2 || formErrors.differentPasswords}
+                helperText={formErrors.differentPasswords ? differentPasswordMessage : passwordReminder}
                 id="repeatNewPassword"
                 type="password"
                 name="repeatNewPassword"
-                placeholder="Escribe aquí tu nueva contraseña"
+                placeholder="Repite aquí tu nueva contraseña"
                 autoFocus
                 required
                 fullWidth
                 variant="filled"
                 // color={emailError ? 'error' : 'primary'}
-                ></TextField>    
+                />  
             <Button type="submit" variant="contained" fullWidth className="mt-4">Enviar</Button>
         </FormGroup>
     </Box>
