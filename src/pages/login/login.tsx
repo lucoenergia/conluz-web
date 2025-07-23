5526555289b655e065cd928fc0174b2d6ba6a83f
éoj <eoj@riseup.net>
2025-07-23 20:13:19 +0200

    [conluzweb-11] Fixed unused vars and corrected types

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -1,5 +1,5 @@
 import { useState, type FC } from "react"
-import { Box, Button, Checkbox, FormControlLabel, FormGroup,InputLabel,Link,TextField, Typography } from "@mui/material"
+import { Box, Button, Checkbox, FormControlLabel, FormGroup, InputLabel, Link, TextField, Typography } from "@mui/material"
 import WavingHandOutlinedIcon from '@mui/icons-material/WavingHandOutlined';
 import { Link as RouterLink, useNavigate } from 'react-router'
 import { login } from "../../api/authentication/authentication";
@@ -25,81 +25,80 @@ export const Login: FC = () => {
       id: !username,
       password: !password,
     };
-    
+
     setFormErrors(newErrors);
 
     return !newErrors.id && !newErrors.password;
   }
-  
-  const handleSubmit = async (data: FormData) => {
-    const id = data.get('id')?.toString().trim();
-    const password = data.get('password')?.toString().trim();
-    
-    if (!validateInput(id,password)) return;
 
+  const handleSubmit = async (data: FormData) => {
+    const id = data.get('id') as string;
+    const password = data.get('password') as string;
+
+    if (!validateInput(id, password)) return;
     try {
-      const response = await login({username: data.get('id'), password: data.get('password')});
+      const response = await login({ username: id.trim(), password: password.trim() });
       if (response && response.token) {
-        setLoginError(false); 
+        setLoginError(false);
         dispatchAuth(response.token);
-        navigate('/');    
+        navigate('/');
       } else {
-        setLoginError(true); 
+        setLoginError(true);
       }
-    } catch(error) {
-      setLoginError(true); 
+    } catch (error) {
+      setLoginError(true);
       console.log(error)
     }
   };
-return <Box component="form" className="p-7 w-full" action={handleSubmit}>
-      <LabeledIcon label={label} icon={WavingHandOutlinedIcon} iconPosition="right" justify="start" variant="compact" labelSize="text-xl"></LabeledIcon>
-      <Typography variant="subtitle2" gutterBottom>
-        Por favor, accede a tu cuenta introduciendo tu DNI/NIF y contraseña
+  return <Box component="form" className="p-7 w-full" action={handleSubmit}>
+    <LabeledIcon label={label} icon={WavingHandOutlinedIcon} iconPosition="right" justify="start" variant="compact" labelSize="text-xl"></LabeledIcon>
+    <Typography variant="subtitle2" gutterBottom>
+      Por favor, accede a tu cuenta introduciendo tu DNI/NIF y contraseña
+    </Typography>
+    {loginError && (
+      <Typography color="error">
+        DNI/NIF o contraseña incorrectos
       </Typography>
-      {loginError && (
-        <Typography color="error">
-          DNI/NIF o contraseña incorrectos
-        </Typography>
-      )}        
-      <FormGroup>
-            <InputLabel className="mt-10">DNI/NIF</InputLabel>
-            <TextField
-                error={formErrors.id}
-                helperText={formErrors.id ? idErrorMessage : ''}
-                id="id"
-                type="text"
-                name="id"
-                placeholder="Escribe aquí tu DNI/NIF"
-                autoFocus
-                required
-                fullWidth
-                variant="filled"
-            />
-            <InputLabel className="mt-4">Contraseña</InputLabel>
-            <PasswordInput
-                error={formErrors.password}
-                helperText={formErrors.password ? passwordErrorMessage : ''}
-                id="password"
-                type="password"
-                name="password"
-                placeholder="Escribe aquí tu contraseña"
-                autoComplete="password"
-                autoFocus
-                required
-                fullWidth
-                variant="filled"
-            />
-            <Box className="flex flex-row justify-between items-center w-full">
-              <FormControlLabel control={<Checkbox color="success"/>} label="Recordarme" className="mt-2"/>
-              <Link component={RouterLink} to='/forgot-password' underline="always" color="info">
-                  {'¿Olvidaste tu contraseña?'}
-              </Link>
-            </Box>
-            <Button type="submit" variant="contained" fullWidth className="mt-4">
-                Entrar
-            </Button>
-            <Typography className="mt-7">¿No puedes acceder a la plataforma?</Typography>
-            <Link component={RouterLink} to='/contact' underline="always" color="info">{'Contacta con nosotres'}</Link>
-        </FormGroup>
-    </Box>
+    )}
+    <FormGroup>
+      <InputLabel className="mt-10">DNI/NIF</InputLabel>
+      <TextField
+        error={formErrors.id}
+        helperText={formErrors.id ? idErrorMessage : ''}
+        id="id"
+        type="text"
+        name="id"
+        placeholder="Escribe aquí tu DNI/NIF"
+        autoFocus
+        required
+        fullWidth
+        variant="filled"
+      />
+      <InputLabel className="mt-4">Contraseña</InputLabel>
+      <PasswordInput
+        error={formErrors.password}
+        helperText={formErrors.password ? passwordErrorMessage : ''}
+        id="password"
+        type="password"
+        name="password"
+        placeholder="Escribe aquí tu contraseña"
+        autoComplete="password"
+        autoFocus
+        required
+        fullWidth
+        variant="filled"
+      />
+      <Box className="flex flex-row justify-between items-center w-full">
+        <FormControlLabel control={<Checkbox color="success" />} label="Recordarme" className="mt-2" />
+        <Link component={RouterLink} to='/forgot-password' underline="always" color="info">
+          {'¿Olvidaste tu contraseña?'}
+        </Link>
+      </Box>
+      <Button type="submit" variant="contained" fullWidth className="mt-4">
+        Entrar
+      </Button>
+      <Typography className="mt-7">¿No puedes acceder a la plataforma?</Typography>
+      <Link component={RouterLink} to='/contact' underline="always" color="info">{'Contacta con nosotres'}</Link>
+    </FormGroup>
+  </Box>
 }
