9fb4e71f42d35031b154c08999894bc6aefa7ccd
Natalia M Zaballos <natmarzab@proton.me>
2025-08-04 13:37:21 +0200

    [conluzweb-3] SupplyDetail page added

---
+++ src/pages/SupplyDetail/SupplyDetailPage.tsx
@@ -0,0 +1,10 @@
+import type { FC } from "react"
+import { Box } from "@mui/material"
+import { useParams } from "react-router";
+
+export const SupplyDetailPage: FC = () => {
+  let { supplyPointId } = useParams();
+
+return <Box className='flex flex-col'>
+</Box>
+}
