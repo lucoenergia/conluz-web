9f99fd2677aafd446c568683036b4df74de93e9b
NatMarZab <81936425+NatMarZab@users.noreply.github.com>
2025-08-05 10:13:34 +0200

    [conluzweb-3] SupplyDetail page added (#54)

--- src/App.tsx
+++ src/App.tsx
@@ -7,9 +7,9 @@ import { LoginLayout } from './layouts/login.layout'
 import { Login } from './pages/login/login'
 import { ForgotPassword } from './pages/ForgotPassword/ForgotPassword'
 import { NewPassword } from './pages/NewPassword/NewPassword'
+import { SupplyDetailPage } from './pages/SupplyDetail/SupplyDetailPage'
 
 function App() {
-
   return (
     <>
       <Routes>
@@ -18,12 +18,14 @@ function App() {
           <Route path='forgot-password'>
             <Route index element={<ForgotPassword/>}></Route>
             <Route path=':token' element={<NewPassword/>}></Route>
-
           </Route>
         </Route> 
         <Route element={<AuthenticatedLayout/>}>
           <Route index />
-          <Route path='supply-points' element={<SupplyPointsPage/>}/>          
+          <Route path='supply-points/'>
+            <Route index element={<SupplyPointsPage/>}></Route>
+            <Route path=':supplyPointId' element={<SupplyDetailPage/>}/>      
+          </Route>    
           <Route path="contact" element={<ContactPage />}/> 
         </Route>
       </Routes>
