15efc8faa16393dd6ddc1a06db674e71b9e2b497
Natalia M Zaballos <natmarzab@proton.me>
2025-08-06 11:18:53 +0200

    [conluzweb-3] GraphCard component added

--- src/pages/SupplyDetail/SupplyDetailPage.tsx
+++ src/pages/SupplyDetail/SupplyDetailPage.tsx
@@ -1,10 +1,66 @@
 import type { FC } from "react"
 import { Box } from "@mui/material"
 import { useParams } from "react-router";
+import { CardList } from "../../components/cardList/CardList";
+import { GraphCard } from "../../components/graphCard/GraphCard";
+import { LoadingGraphCard } from "../../components/graphCard/LoadingGraphCard";
 
 export const SupplyDetailPage: FC = () => {
   let { supplyPointId } = useParams();
 
+
+
+const measurementData = [
+  {
+    name:  'Producción Asignada',
+    value:  [],
+  },
+   {
+    name: 'Consumo de red',
+    value: [], 
+  },
+  {
+    name: 'Autoconsumo',
+    value:  [], 
+  },
+  {
+    name: 'Excedente',
+    value: [], 
+  }
+]
+
+const assignedProdText:string = 'Cantidad de energía generada por la comunidad que te ha sido asignada a este punto de suministro en base a su coeficiente de reparto'
+const networkUsage:string = 'Cantidad de energía consumida de la red'
+const selfConsumption:string = 'Cantidad de energía autoconsumida'
+const excedent:string = 'Cantidad de energía no consumida'
+
 return <Box className='flex flex-col'>
+      {/* <CardList className='flex flex-row flex-wrap'> */}
+      <CardList className='flex flex-row'>
+
+      {measurementData.map((item) => (
+        <GraphCard 
+          title={item.name} 
+          infoText={
+            item.name === 'Producción Asignada' ? assignedProdText :
+            item.name === 'Consumo de red' ? networkUsage :
+            item.name === 'Autoconsumo' ? selfConsumption :
+            item.name === 'Excedente' ? excedent : ''
+          }
+          className="w-full md:w-1/2 h-64 m-[20px] p-2">
+            <Box className="">GRÁFICA</Box>
+        </GraphCard>
+      ))}
+    </CardList>
+  {/* {!isLoading && !error &&
+
+  }
+  {isLoading &&
+    <CardList>
+      {
+        Array.from({ length: 5}).map(() => <LoadingGraphCard/>)
+      }
+    </CardList>
+  }   */}
 </Box>
 }
