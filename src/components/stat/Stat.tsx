b6aca3ab153537a07a111904389f855300aa3f23
éoj <eoj@riseup.net>
2025-08-07 09:24:08 +0200

    [conluzweb-3] Added stat component

---
+++ src/components/stat/Stat.tsx
@@ -0,0 +1,18 @@
+import { Typography } from "@mui/material";
+import type { FC } from "react";
+
+
+interface StatProps {
+  label: string,
+  value: string,
+  variant?: 'normal' | 'big'
+}
+
+export const Stat: FC<StatProps> = ({label, value, variant='normal'}) => {
+  return (
+    <div>
+        <Typography className={variant === 'normal' ? "font-semibold" : "font-light"}>{label}</Typography>
+        <Typography className={variant === 'normal' ? 'text-sm font-light' : 'text-2xl font-bold'}>{value}</Typography>
+    </div>
+  )
+}
