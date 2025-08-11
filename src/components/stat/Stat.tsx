621d1ca4183e9d9f4b4ac6f7b0fc60c081f40ac6
éoj <eoj@riseup.net>
2025-08-11 07:41:27 +0200

    [conluzweb-3] Added SupplyStatsCard component

--- src/components/stat/Stat.tsx
+++ src/components/stat/Stat.tsx
@@ -5,12 +5,19 @@ import type { FC } from "react";
 interface StatProps {
   label: string,
   value: string,
-  variant?: 'normal' | 'big'
+  variant?: 'normal' | 'big',
+  className?: string
 }
 
-export const Stat: FC<StatProps> = ({label, value, variant='normal'}) => {
+const textAlign = {
+  left: 'text-left',
+  right: 'text-right',
+  center: 'text-center'
+} as const
+
+export const Stat: FC<StatProps> = ({ label, value, variant='normal', className}) => {
   return (
-    <div>
+    <div className={className}>
         <Typography className={variant === 'normal' ? "font-semibold" : "font-light"}>{label}</Typography>
         <Typography className={variant === 'normal' ? 'text-sm font-light' : 'text-2xl font-bold'}>{value}</Typography>
     </div>
