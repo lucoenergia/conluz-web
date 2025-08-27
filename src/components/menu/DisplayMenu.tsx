c9c9fe6a6bd54b3ed859ef1f0a30a583f1c1afb1
Natalia M Zaballos <natmarzab@proton.me>
2025-08-27 11:08:33 +0200

    [conluzweb-8] Basic modal and DisablePointModal created (#54, #57)

--- src/components/menu/DisplayMenu.tsx
+++ src/components/menu/DisplayMenu.tsx
@@ -1,5 +1,5 @@
 import { useState, type FC } from "react"
-import { Divider, IconButton } from "@mui/material"
+import { Divider, IconButton, MenuItem } from "@mui/material"
 import { MenuTemplate } from "./MenuTemplate"
 import { MenuLinkItem } from "./MenuLinkItem"
 import { LabeledIcon } from "../labeled-icon/LabeledIcon"
@@ -7,15 +7,20 @@ import MoreVertIcon from '@mui/icons-material/MoreVert';
 import AssessmentOutlinedIcon from '@mui/icons-material/AssessmentOutlined';
 import ModeEditOutlineOutlinedIcon from '@mui/icons-material/ModeEditOutlineOutlined';
 import NotInterestedOutlinedIcon from '@mui/icons-material/NotInterestedOutlined';
+import { DisablePointModal } from "../modals/DisablePointModal"
+
+interface DisplayMenuProps {
+    code: string,
+    disableSupplyPoint: (code:string) => void
+}
 
 interface DisplayMenuProps {
   supplyPointId: string
 }
 
-export const DisplayMenu: FC<DisplayMenuProps> = ({ supplyPointId }) => {
+export const DisplayMenu: FC<DisplayMenuProps><DisplayMenuProps> = ({ supplyPointId }{code, disableSupplyPoint}) => {
   const [anchorElement, setAnchorElement] = useState<HTMLElement | null>(null);
-
-  const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
+const [isModalOpen, setIsModalOpen] = useState(false);  const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
     event.preventDefault();
     setAnchorElement(event.currentTarget);
   };
@@ -24,6 +29,18 @@ export const DisplayMenu: FC<DisplayMenuProps> = ({ supplyPointId }) => {
     event.preventDefault();
     setAnchorElement(null);
   };
+
+const handleOpen = () => {
+console.log('click en deshabilitar');
+console.log('code ', code);
+setIsModalOpen(true);
+handleCloseUserMenu();
+}
+
+const handleCloseModal = () => {
+setIsModalOpen(false);
+};
+
 
   return <>
     <IconButton onClick={handleOpenUserMenu}><MoreVertIcon /></IconButton>
@@ -49,14 +66,19 @@ export const DisplayMenu: FC<DisplayMenuProps> = ({ supplyPointId }) => {
           label="Editar" />
       </MenuLinkItem>
       <Divider />
-      <MenuLinkItem to="/disable">
+      <MenuItem onClick={handleOpen}>
         <LabeledIcon
           variant="compact"
           justify="between"
           iconPosition="right"
           icon={NotInterestedOutlinedIcon}
           label="Deshabilitar" />
-      </MenuLinkItem>
+      </MenuItem>
+            <DisablePointModal 
+                isOpen={isModalOpen} 
+                code={code} 
+                disableSupplyPoint={disableSupplyPoint}
+                onClose={handleCloseModal}/>
     </MenuTemplate>
   </>
 }
