afd4e3a05dbda018187bc52f76d2b921f5b15cbc
éoj <eoj@riseup.net>
2025-08-28 09:42:32 +0200

    [conluzweb-3] Added missing loading state components

---
+++ src/components/stat/LoadingStat.tsx
@@ -0,0 +1,18 @@
+import { Skeleton, Typography } from "@mui/material";
+import type { FC } from "react";
+
+
+interface StatProps {
+  label: string,
+  variant?: 'normal' | 'big',
+  className?: string
+}
+
+export const LoadingStat: FC<StatProps> = ({ label, variant='normal', className}) => {
+  return (
+    <div className={className}>
+        <Typography className={variant === 'normal' ? "font-semibold" : "font-light"}>{label}</Typography>
+        <Typography className={variant === 'normal' ? 'text-sm font-light' : 'text-2xl font-bold'}><Skeleton/></Typography>
+    </div>
+  )
+}
